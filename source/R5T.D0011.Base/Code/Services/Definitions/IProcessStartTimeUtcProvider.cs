69e26efab22bc69000d0ee0817435edce3cc08c2
MinexAutomation <minex.automation@gmail.com>
2020-05-15 16:41:01 -0700

    Added R5T.D0011.Base library.

---
+++ source/R5T.D0011.Base/Code/Services/Definitions/IProcessStartTimeUtcProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0011
+{
+    public interface IProcessStartTimeUtcProvider
+    {
+        Task<DateTime> GetProcessStartTimeUtcAsync();
+    }
+}
---
+++ source/R5T.D0011.Base/Code/Services/Extensions/IProcessStartTimeUtcProviderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0011
+{
+    public static class IProcessStartTimeUtcProviderExtensions
+    {
+        public static async Task<DateTime> GetProcessStartTimeLocalAsync(this IProcessStartTimeUtcProvider processStartTimeUtcProvider)
+        {
+            var processStartTimeUtc = await processStartTimeUtcProvider.GetProcessStartTimeUtcAsync();
+
+            var processStartTimeLocal = processStartTimeUtc.ToLocalTime();
+            return processStartTimeLocal;
+        }
+    }
+}
