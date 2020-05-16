82f493c9e230cf24c497b0dc59a06bc90080da74
MinexAutomation <minex.automation@gmail.com>
2020-05-15 17:21:17 -0700

    Added R5T.D0011.Standard library.

---
+++ source/R5T.D0011.Standard/Code/Services/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+using R5T.D0011.Default;
+
+
+namespace R5T.D0011.Standard
+{
+    public static class IServiceCollectionExtension
+    {
+        /// <summary>
+        /// Adds the <see cref="IProcessStartTimeUtcProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddProcessStartTimeUtcProvider(this IServiceCollection services)
+        {
+            services.AddDefaultProcessStartTimeUtcProvider();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IProcessStartTimeUtcProvider"/> service.
+        /// </summary>
+        public static ServiceAction<IProcessStartTimeUtcProvider> AddProcessStartTimeUtcProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction<IProcessStartTimeUtcProvider>.New(() => services.AddProcessStartTimeUtcProvider());
+            return serviceAction;
+        }
+    }
+}
